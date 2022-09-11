d3e77252006f3c524fee33242207007b4bb7ff64
Batuhan Onder <43814082+BatuhanOnder@users.noreply.github.com>
2022-09-12 01:29:11 +0300

    ADD ServiceExtension class

--- emplooye-api/EmployeeManagement.Api/Extensions/ServiceExtension.cs
+++ emplooye-api/EmployeeManagement.Api/Extensions/ServiceExtension.cs
@@ -1,6 +1,48 @@
-﻿namespace EmployeeManagement.Api.Extensions;
+﻿
 
-public class ServiceExtension
+namespace EmployeeManagement.Api.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+public static class ServiceExtension
 {
+
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddControllers().AddJsonOptions(
+            options => options.JsonSerializerOptions.PropertyNamingPolicy = null);;
+
+        services.AddHealthChecks();
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+    }
+    public static void AddScopes(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IMongoDatabase>(options => {
+            var settings =  configuration.GetSection("EmployeeManagementDatabase").Get<EmployeeManagementDatabaseSettings>();
+            var client = new MongoClient(settings.ConnectionString);
+            return client.GetDatabase(settings.DatabaseName);
+        });
+
+        services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+    }
+
+    public static void AddCorsConfiguration(this IServiceCollection services)
+    {
+        services.AddCors(options => options.AddPolicy("ApiCorsPolicy", corsPolicyBuilder =>
+        {
+            corsPolicyBuilder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+        }));
+    }
+
+    public static void AddMediatr(this IServiceCollection services)
+    {
+       services.AddMediatR(typeof(GetEmployeesQuery).Assembly);
+    }
     
-}
+    
+    
+   
+ }
--- emplooye-api/EmployeeManagement.Api/Program.cs
+++ emplooye-api/EmployeeManagement.Api/Program.cs
@@ -1,29 +1,13 @@
+using EmployeeManagement.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<IMongoDatabase>(options => {
-    var settings =  builder.Configuration.GetSection("EmployeeManagementDatabase").Get<EmployeeManagementDatabaseSettings>();
-    var client = new MongoClient(settings.ConnectionString);
-    return client.GetDatabase(settings.DatabaseName);
-});
-
-builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", corsPolicyBuilder =>
-{
-    corsPolicyBuilder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
-}));
-builder.Services.AddMediatR(typeof(GetEmployeesQuery).Assembly);
-
-builder.Services.AddControllers().AddJsonOptions(
-    options => options.JsonSerializerOptions.PropertyNamingPolicy = null);;
-
-builder.Services.AddHealthChecks();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScopes(builder.Configuration);
+builder.Services.AddCorsConfiguration();
+builder.Services.AddMediatr();
+builder.Services.AddServices();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -32,7 +16,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors("ApiCorsPolicy");  
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 var option = new RewriteOptions();
